067a7b13ea7bf69d687cce18f5b3f1f8442f7804
juasanpar <juasanpar@hotmail.com>
2018-03-21 19:21:58 +0100

    Añadidos

--- socket_cliente.py
+++ socket_cliente.py
@@ -11,7 +11,8 @@ nonce_cliente = None
 mensaje = b''
 
 s.connect((host, port))
-print('Se ha conectado')
+print('-----------------Estableciendo conexion como cliente-----------------')
+
 # Recibe el nonce del servidor #
 while True:
     rec = ''
@@ -22,27 +23,31 @@ while True:
             nonces[1] = nonce_serv
         print('Se ha recibido el nonce del servidor : ' + str(nonce_serv))
         break
+    
 # Genera y manda su nonce al servidor #
 nonces[0] = generate_nonces(used_nonces)
 sleep(5)
 s.send(bytes(str(nonces[0]).encode('ascii')))
+
 # Manda el mensaje al servidor #
 while True:
     mensaje_local = mensaje_transaccion('Mi cuenta', 'Tu cuenta', 100, nonces[1])
-    mensaje = mensaje_local
+    mensajeReplay = mensaje_local
+    mensajeMITM =  mensaje_transaccion('Mi cuenta', 'Tu cuenta', 200, nonces[1])
     s.send(mensaje_local)
-    print('Se ha enviado el mensaje')
+    print('Se ha enviado el mensaje '+mensaje)
     break
+
 s.close()
-
-
 
 sleep(7)
 # Se abre una nueva conexion #
 s = socket.socket()
+
 # A partir de aqui el cliente es el hacker  #
 s.connect((host, port))
-print('Se ha conectado')
+print('---------------Estableciendo conexion como atacante--------------------')
+
 # Recibe el nonce del servidor #
 while True:
     rec = ''
@@ -51,16 +56,23 @@ while True:
         nonce_serv = int(rec.decode())
         if nonce_serv:
             nonces[1] = nonce_serv
-        print('Se ha recibido el nonce del servidor : ' + str(nonce_serv))
+        print('Se ha recibido el nonce del servidor : ' + str(nonce_serv)+"\n")
         break
 # Genera y manda su nonce al servidor #
 sleep(5)
 nonces[0] = generate_nonces(used_nonces)
 s.send(bytes(str(nonces[0]).encode('ascii')))
 sleep(5)
+
 # Manda el mensaje al servidor #
-while True:
-    s.send(mensaje)
-    print('Se ha enviado el mensaje')
-    break
+print "Enviando mensaje para simular ataque de replay"
+print "Dicho mensaje es el mismo que el enviado anteriormente, con el mismo nonce"
+s.send(mensajeReplay)
+print('Se ha enviado el mensaje '+mensajeReplay+"\n")
+
+print "Enviando mensaje para simular ataque de MITM"
+print "Dicho mensaje es distinto que el enviado anteriormente, con el mismo nonce"
+s.send(mensajeMITM)
+print('Se ha enviado el mensaje '+mensajeMITM)
+
 s.close()
--- socket_servidor.py
+++ socket_servidor.py
@@ -1,7 +1,7 @@
 import socket
 from transacciones import check_mensaje, get_cantidad, generate_nonces
 from time import sleep
-
+import sys 
 # Se crea objeto socket
 s = socket.socket()
 # Se obtiene el nombre local de la maquina
@@ -15,15 +15,18 @@ used_nonces = []
 
 dinero = 1000
 
-
 # Se espera a que el cliente se conecte
 s.listen(5)
+print "----------Esperando conexion del cliente-----------"
+
 # connection es un nuevo socket para mandar y recibir. address es la direccion del otro socket en la conexion
 connection, address = s.accept()
 sleep(5)
+
 # El servidor genera y manda su nonce #
 nonces[0] = generate_nonces(used_nonces)
 connection.send(bytes(str(nonces[0]).encode('ascii')))
+
 # Recibe el nonce del cliente #
 while True:
     rec = ''
@@ -34,6 +37,7 @@ while True:
             nonces[1] = nonce_cliente
         print('Se ha recibido el nonce del cliente : ' + str(nonce_cliente))
         break
+    
 # El servidor recibe el mensaje del cliente #
 while True:
     rec = ''
@@ -43,24 +47,32 @@ while True:
         if check:
             local_dinero = dinero
             dinero = local_dinero + get_cantidad(rec)
-        print('Se ha recibido el mensaje')
+        print('Se ha recibido el mensaje '+rec)
         print('Dinero en cuenta : ' + str(dinero))
-        print(rec)
         break
+    
 # Antes de cerrar la conexion, se marca el nonce del servidor como usado #
 used_nonces.append(nonces[0])
 nonces = [None, None]
 connection.close()
 
+print "--------------Abriendo nueva conexion con atacante-----------------"
+print "En estos momentos el nonce del servidor es distinto, por lo que este"
+print "dectectara mensajes enviados con nonces anteriores, es decir, detectara"
+print "cuando se produce un ataque."
+
 # Se abre una nueva conexion #
 s = socket.socket()
 s.bind((host, port))
+
 # Se inicia la conexion con el atacante #
 s.listen(20)
 connection, address = s.accept()
+
 # El servidor genera y manda su nonce #
 nonces[0] = generate_nonces(used_nonces)
 connection.send(bytes(str(nonces[0]).encode('ascii')))
+
 # Recibe el nonce del cliente #
 while True:
     rec = ''
@@ -69,21 +81,29 @@ while True:
         nonce_cliente = int(rec.decode())
         if nonce_cliente:
             nonces[1] = nonce_cliente
-        print('Se ha recibido el nonce del cliente : ' + str(nonce_cliente))
+        print('Se ha recibido el nonce del cliente : ' + str(nonce_cliente)+"\n")
         break
+    
+i = 0   
 while True:
-    print(dinero)
+    i+=1
+    print("Dinero en la cuenta "+str(dinero))
     rec = ''
     rec = connection.recv(1024)
+    print "Se ha recibido el mensaje "+rec
+    
     if rec:
         check = check_mensaje(rec, nonces[0])
+        
         if check:
             local_dinero = dinero
             dinero = local_dinero + get_cantidad(rec)
         else:
-            print('Se ha detectado un ataque')
-        print('Se ha recibido el mensaje')
-        print('Dinero en cuenta : ' + str(dinero))
-        print(rec)
+            print >>sys.stderr, 'Se ha detectado un ataque, mensaje '+rec+" descartado"
+            
+        print('Dinero en cuenta : ' + str(dinero)+"\n")
+        
+    if (i == 2):
         break
+        
 connection.close()
