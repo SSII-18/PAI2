54e3aaf04095b1e44e4c42c95a08f780fe329488
juasanpar <juasanpar@hotmail.com>
2018-03-22 15:38:55 +0100

    Cambios

--- socket_cliente.py
+++ socket_cliente.py
@@ -17,6 +17,7 @@ clave_publica_servidor = None
 s.connect((host, port))
 print('-----------------Estableciendo conexion como cliente-----------------')
 print('Se ha conectado')
+
 # Recibe el nonce del servidor #
 while True:
     rec = ''
@@ -27,10 +28,12 @@ while True:
             nonces[1] = nonce_serv
         print('Se ha recibido el nonce del servidor : ' + str(nonce_serv))
         break
+    
 # Genera y manda su nonce al servidor #
 nonces[0] = generate_nonces(used_nonces)
 sleep(5)
 s.send(bytes(str(nonces[0]).encode('ascii')))
+
 # Recibe la clave publica del servidor #
 while True:
     rec = ''
@@ -40,11 +43,13 @@ while True:
         clave_publica_servidor = RSA.importKey(rec)
         print('Se ha recibido la clave publica del servidor : ' + str(clave_publica_servidor))
         break
+    
 # Genera sus claves y manda su clave publica al servidor #
 clave_privada_cliente, clave_publica_cliente = generar_claves_firma()
 export_publica = clave_publica_cliente.exportKey('DER')
 s.send(export_publica)
 sleep(5)
+
 # Manda el mensaje al servidor #
 while True:
     mensaje_local = mensaje_transaccion('Mi cuenta', 'Tu cuenta', 100, nonces[1], clave_privada_cliente)
@@ -52,6 +57,7 @@ while True:
     s.send(mensaje_local)
     print('Se ha enviado el mensaje')
     break
+
 # Se espera confirmacion #
 while True:
     rec = ''
@@ -64,48 +70,23 @@ s.close()
 print('Conexion cerrada')
 
 
-
 sleep(10)
 # Se abre una nueva conexion #
 s = socket.socket()
 print('---------------Interceptando la conexion como atacante--------------------')
+
 # A partir de aqui el cliente es el hacker  #
 s.connect((host, port))
 print('Se ha conectado')
-# Recibe el nonce del servidor #
-while True:
-    rec = ''
-    rec = s.recv(1024)
-    if rec:
-        nonces[1] = int(rec.decode())
-        if nonces[1]:
-            print('Se ha recibido el nonce del servidor : ' + str(nonces[1]))
-            break
-# Genera y manda su nonce al servidor #
-sleep(5)
-nonces[0] = generate_nonces(used_nonces)
-s.send(bytes(str(nonces[0]).encode('ascii')))
-# Recibe la clave publica del servidor #
-while True:
-    rec = ''
-    rec = s.recv(10240)
-    if rec:
-        print(rec)
-        clave_publica_servidor = RSA.importKey(rec)
-        print('Se ha recibido la clave publica del servidor : ' + str(clave_publica_servidor))
-        break
-# Genera sus claves y manda su clave publica al servidor #
-clave_privada_cliente, clave_publica_cliente = generar_claves_firma()
-export_publica = clave_publica_cliente.exportKey('DER')
-s.send(export_publica)
-sleep(5)
+
 # Manda el mensaje al servidor #
 while True:
     print ("Enviando mensaje para simular ataque de replay")
     print ("Dicho mensaje es el mismo que el enviado anteriormente, con el mismo nonce")
-    mensajeReplay = 'Mi cuenta, Tu cuenta, 100, '+ nonce_serv
+    mensajeReplay = 'Mi cuenta, Tu cuenta, 100, '+mensaje.split(',')[3]
     s.send(mensajeReplay)
     print('Se ha enviado el mensaje '+mensajeReplay+"\n")
+    
 # Se espera confirmacion #
 while True:
     rec = ''
@@ -113,13 +94,15 @@ while True:
     if rec:
         print(rec.decode())
         break
+    
 # Manda mensaje al servidor    
 while True:
     print ("Enviando mensaje para simular ataque de MITM")
     print ("Dicho mensaje es distinto que el enviado anteriormente, con el mismo nonce")
-    mensajeMITM = 'Mi cuenta, Tu cuenta, 100000, '+nonce_serv
+    mensajeMITM = 'Mi cuenta, Tu cuenta, 100000, '+mensaje.split(',')[3]
     s.send(mensajeMITM)
     print('Se ha enviado el mensaje '+mensajeMITM)
+    
 # Se espera confirmacion #
 while True:
     rec = ''
--- socket_servidor.py
+++ socket_servidor.py
@@ -5,7 +5,6 @@ from time import sleep
 import sys
 from Crypto.PublicKey import RSA
 
-
 def checkMensajes(j): 
     global dinero
     global mensajesIntegros
@@ -45,10 +44,13 @@ def checkMensajes(j):
 
 # Se crea objeto socket
 s = socket.socket()
+
 # Se obtiene el nombre local de la maquina
 host = socket.gethostbyaddr('127.0.0.1')[0]
+
 # Se reserva un puerto para el socket
 port = 31415
+
 # Se unen el nombre de la maquina y el puerto
 s.bind((host, port))
 nonces = [None, None]
@@ -64,12 +66,15 @@ mensajesNoIntegros = 0
 
 # Se espera a que el cliente se conecte
 s.listen(5)
+
 # connection es un nuevo socket para mandar y recibir. address es la direccion del otro socket en la conexion
 connection, address = s.accept()
 sleep(5)
+
 # El servidor genera y manda su nonce #
 nonces[0] = generate_nonces(used_nonces)
 connection.send(bytes(str(nonces[0]).encode('ascii')))
+
 # Recibe el nonce del cliente #
 while True:
     rec = ''
@@ -80,10 +85,12 @@ while True:
             nonces[1] = nonce_cliente
         print('Se ha recibido el nonce del cliente : ' + str(nonce_cliente))
         break
+    
 # El servidor manda su clave publica al cliente #
 clave_privada_servidor, clave_publica_servidor = generar_claves_firma()
 export_publica = clave_publica_servidor.exportKey('DER')
 connection.send(export_publica)
+
 # El servidor recibe la clave publica del cliente #
 while True:
     rec = ''
@@ -93,46 +100,23 @@ while True:
         clave_publica_cliente = RSA.importKey(rec)
         print('Se ha recibido la clave publica del cliente : ' + str(clave_publica_cliente))
         break
+    
 # El servidor recibe el mensaje del cliente #
 checkMensajes(1)
+
 # Antes de cerrar la conexion, se marca el nonce del servidor como usado #
 used_nonces.append(nonces[0])
-nonces = [None, None]
 connection.close()
 sleep(5)
 
 # Se abre una nueva conexion #
 s = socket.socket()
 s.bind((host, port))
+
 # Se inicia la conexion con el atacante #
 s.listen(20)
 connection, address = s.accept()
-# El servidor genera y manda su nonce #
-nonces[0] = generate_nonces(used_nonces)
-connection.send(bytes(str(nonces[0]).encode('ascii')))
-# Recibe el nonce del cliente #
-while True:
-    rec = ''
-    rec = connection.recv(1024)
-    if rec:
-        nonce_cliente = int(rec.decode())
-        if nonce_cliente:
-            nonces[1] = nonce_cliente
-        print('Se ha recibido el nonce del cliente : ' + str(nonce_cliente))
-        break
-# El servidor manda su clave publica al cliente #
-clave_privada_servidor, clave_publica_servidor = generar_claves_firma()
-export_publica = clave_publica_servidor.exportKey('DER')
-connection.send(export_publica)
-# El servidor recibe la clave publica del cliente #
-while True:
-    rec = ''
-    rec = connection.recv(10240)
-    if rec:
-        print(rec)
-        clave_publica_cliente = RSA.importKey(rec)
-        print('Se ha recibido la clave publica del cliente : ' + str(clave_publica_cliente))
-        break
+
 # El servidor recibe el mensaje #
 checkMensajes(2)
 
